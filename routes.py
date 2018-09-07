81b8c27108aa975301ca5d57cfb130d1cbc1a9c1
Michelle Ho <michelleho@Michelles-MacBook-Pro-2.local>
2018-09-07 00:10:28 -0400

    testing access

--- routes.py
+++ routes.py
@@ -1,3 +1,4 @@
+# this is learning flask
 from flask import Flask, render_template, request, session, url_for, redirect
 from models import db, User, Place
 from forms import SignupForm, LoginForm, AddressForm
